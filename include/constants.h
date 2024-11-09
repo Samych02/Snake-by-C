cd022a48ed1c20c21d9b679f0daab9a9f117859b
samych02 <cherif.samy1000@gmail.com>
2024-11-09 13:57:53 +0100

    commit

--- include/constants.h
+++ include/constants.h
@@ -6,9 +6,10 @@
 #define BACKGROUND_COLOR    {255, 255, 255, 1}
 #define SNAKE0_COLOR        {40, 96, 53, 1}
 #define SNAKE1_COLOR        {40, 147, 53, 1}
+#define BLACK_COLOR         {0, 0, 0, 1}
 
 // rendering values
 // since we are working with squares, no need for width and height
-#define SCREEN_LENGTH  400
+#define SCREEN_LENGTH  416
 
-#define BOARD_LENGTH  25
+#define BOARD_LENGTH  26
